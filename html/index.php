d01935619b4cd7b46816638433e2b6af3cb40194
Andrew Martin <andy123@stanford.edu>
2020-12-10 14:05:26 -0800

    more docs

--- html/index.php
+++ html/index.php
@@ -1,1 +1,11 @@
 <?php phpinfo();
+
+
+// Capture a DET push
+// http://cups/webhook.php
+// API query of that record to get the PDF
+// print the pdf by saving it to /tmp and exec 'lp -d Ricoh3500_ENT filename.pdf'
+
+// Add the UI phpCap to interact via api with redcap
+
+
