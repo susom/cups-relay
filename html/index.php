704e17ab34d340887b648ee800224e64ef311cb8
Andrew Martin <andy123@stanford.edu>
2021-04-22 12:34:26 -0700

    added new action that supports printing of a file-upload field from redcap

--- html/index.php
+++ html/index.php
@@ -42,51 +42,94 @@ try {
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // We have an incoming request
         if (!empty($_POST)) {
-            // Post contains body
-            $record_id = filter_var(@$_POST['record_id'], FILTER_SANITIZE_STRING);
-            $event_name = filter_var(@$_POST['event_name'], FILTER_SANITIZE_STRING);
-            $instruments = @$_POST['instruments'];
-            $compact_display = (bool) filter_var(@$_POST['compact_display'], FILTER_SANITIZE_NUMBER_INT);
+            $action = filter_var($_POST['action'], FILTER_SANITIZE_STRING);
 
-            if (empty($record_id) || empty($event_name) || empty($instruments)) {
-                throw new Exception("Missing required input(s) - see logs");
-                Plugin::log("Invalid inputs", $_POST);
-            }
+            // PRINT A SINGLE FIELD
+            if ($action == "print_file_field") {
+                $record_id = filter_var(@$_POST['record_id'], FILTER_SANITIZE_STRING);
+                $event_name = filter_var(@$_POST['event_name'], FILTER_SANITIZE_STRING);
+                $field_name = filter_var(@$_POST['field_name'], FILTER_SANITIZE_STRING);
 
-            $project = new RedCapProject($apiUrl, $apiToken);
-            $projectInfo = $project->exportProjectInfo();
-            //print "<pre>" . print_r($projectInfo, true) . "</pre>";
-
-            foreach ($instruments as $instrument) {
-                $instrument = filter_var($instrument, FILTER_SANITIZE_STRING);
-                Plugin::log($record_id, $event_name, $instrument);
-                $file = "/tmp/" . $record_id . "_" . $event_name . "_" . $instrument . ".pdf";
-                $q = $project->exportPdfFileOfInstruments($file, $record_id, $event_name, $instrument, null, $compact_display);
-                // See if result is error
-                $q = json_decode($q,true);
-                if (json_last_error() == JSON_ERROR_NONE) {
-                    // We got json which means an error
-                    $errorMsg = empty($q['error']) ? json_encode($q) : $q['error'];
-                    throw new Exception( $errorMsg );
-                } else {
-                    // Result was not json meaning likely a PDF - so let's print the file
-                    $output=null;
-                    $retval=null;
-                    //$options = "-o number-up=2 -o sides=two-sided-long-edge ";
-                    $options = "-o sides=two-sided-long-edge ";
-                    $cmd = 'lp -d ' . $printerName . ' ' . $options . $file;
-                    if ($testMode) {
-                        Plugin::log("TESTMODE: $file would have been printed to $printerName");
-                    } else {
-                        exec( $cmd, $output, $retval);
-                        Plugin::log($cmd, $output, $retval);
-                        Plugin::log($file . " printed to $printerName");
-                    }
-                    // $result = ['success' => "$instrument printed"];
-                    unlink($file);
+                if (empty($record_id) || empty($event_name) || empty($field_name)) {
+                    throw new Exception("Missing required input(s) - see logs");
+                    Plugin::log("Invalid inputs for $action", $_POST);
                 }
+
+                $project = new RedCapProject($apiUrl, $apiToken);
+                // $projectInfo = $project->exportProjectInfo();
+                $file     = "/tmp/" . $record_id . "_" . $event_name . "_" . $field_name . ".pdf";
+                $contents = $project->exportFile($record_id,$field_name,$event_name);
+
+                Plugin::log("Obtained $file with size " . filesize($file));
+                file_put_contents($file, $contents);
+
+                $output=null;
+                $retval=null;
+                //$options = "-o number-up=2 -o sides=two-sided-long-edge ";
+                $options = "-o sides=two-sided-long-edge ";
+                $cmd = 'lp -d ' . $printerName . ' ' . $options . $file;
+                if ($testMode) {
+                    Plugin::log("TESTMODE: $file would have been printed to $printerName");
+                } else {
+                    exec( $cmd, $output, $retval);
+                    Plugin::log($cmd, $output, $retval);
+                    Plugin::log($file . " printed to $printerName");
+                }
+                // $result = ['success' => "$instrument printed"];
+                unlink($file);
+                $result = ["success" => "$file printed on $printerName"];
             }
-            $result = ["success" => implode(", ", $instruments) . " printed on $printerName"];
+            // DO THE PREVIOUS PRINT FORMS BEHAVIOR
+            else {
+
+                // Post contains body
+                $record_id = filter_var(@$_POST['record_id'], FILTER_SANITIZE_STRING);
+                $event_name = filter_var(@$_POST['event_name'], FILTER_SANITIZE_STRING);
+                $instruments = @$_POST['instruments'];
+                $compact_display = (bool) filter_var(@$_POST['compact_display'], FILTER_SANITIZE_NUMBER_INT);
+
+                if (empty($record_id) || empty($event_name) || empty($instruments)) {
+                    throw new Exception("Missing required input(s) - see logs");
+                    Plugin::log("Invalid inputs", $_POST);
+                }
+
+                $project = new RedCapProject($apiUrl, $apiToken);
+                $projectInfo = $project->exportProjectInfo();
+                //print "<pre>" . print_r($projectInfo, true) . "</pre>";
+
+                foreach ($instruments as $instrument) {
+                    $instrument = filter_var($instrument, FILTER_SANITIZE_STRING);
+                    Plugin::log($record_id, $event_name, $instrument);
+                    $file = "/tmp/" . $record_id . "_" . $event_name . "_" . $instrument . ".pdf";
+                    $q = $project->exportPdfFileOfInstruments($file, $record_id, $event_name, $instrument, null, $compact_display);
+                    // See if result is error
+                    $q = json_decode($q,true);
+                    if (json_last_error() == JSON_ERROR_NONE) {
+                        // We got json which means an error
+                        $errorMsg = empty($q['error']) ? json_encode($q) : $q['error'];
+                        throw new Exception( $errorMsg );
+                    } else {
+                        // Result was not json meaning likely a PDF - so let's print the file
+                        $output=null;
+                        $retval=null;
+                        //$options = "-o number-up=2 -o sides=two-sided-long-edge ";
+                        $options = "-o sides=two-sided-long-edge ";
+                        $cmd = 'lp -d ' . $printerName . ' ' . $options . $file;
+                        if ($testMode) {
+                            Plugin::log("TESTMODE: $file would have been printed to $printerName");
+                        } else {
+                            exec( $cmd, $output, $retval);
+                            Plugin::log($cmd, $output, $retval);
+                            Plugin::log($file . " printed to $printerName");
+                        }
+                        // $result = ['success' => "$instrument printed"];
+                        unlink($file);
+                    }
+                }
+                $result = ["success" => implode(", ", $instruments) . " printed on $printerName"];
+
+            }
+
         } else {
             throw new Exception("Post is empty");
         }
